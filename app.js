7ffbacce5ccdb2180468ec8afd5c14e0ce7ba6a7
BenjaminFalcon27 <85941055+BenjaminFalcon27@users.noreply.github.com>
2023-03-21 13:23:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var indexRouter = require("./routes/index");
+var studentsRouter = require("./routes/student");
+
+var app = express();
+// Intégration de la bdd
+var connectionString =
+  "mongodb+srv://admin:vXo5t0Qvqw7oDUkJ@iut.zfjaw8n.mongodb.net/test";
+var mongoDB = process.env.MONGODB_URI || connectionString;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/students", studentsRouter);
+
+module.exports = app;
