e901a27973d36c2dff8d7100634933098fc705c7
BenjaminFalcon27 <85941055+BenjaminFalcon27@users.noreply.github.com>
2023-03-27 14:09:17 +0200

    add: cors

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var studentsRouter = require("./routes/student");
 
 var app = express();
+
 // Intégration de la bdd
 var connectionString =
   "mongodb+srv://admin:vXo5t0Qvqw7oDUkJ@iut.zfjaw8n.mongodb.net/test";
@@ -22,6 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/students", studentsRouter);
